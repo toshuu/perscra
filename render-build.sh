fb60ad4499979c9e903c8c94c7c09bef56841d32
toshuu <toshuu.ps4@gmail.com>
2025-02-08 23:04:00 +0530

    RenderSupport3

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-export PUPPETEER_SKIP_DOWNLOAD=true
-npm install puppeteer
-npx --yes puppeteer browsers install chrome || true
+export PUPPETEER_EXECUTABLE_PATH="/opt/render/.cache/puppeteer/chrome/linux-121.0.6167.85/chrome-linux64/chrome"
+npm install
+npm start
