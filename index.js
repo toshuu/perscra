18bf7f5e26fc47db0648bec2bc8dff90fbc72d82
toshuu <toshuu.ps4@gmail.com>
2025-02-08 22:32:58 +0530

    RenderSupport

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/scrape", async (req, res) => {
     try {
         browser = await puppeteer.launch({
             headless: "new",
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+            args: ["--no-sandbox", "--disable-setuid-sandbox",  "--disable-dev-shm-usage"],
         });
 
         const page = await browser.newPage();
